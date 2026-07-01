5398b1f37c847a9eda3c9858082944c8db227844
Daniel Sin <67078201+dsin85691@users.noreply.github.com>
2026-07-01 11:28:31 -0400

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = {} 
+        for i in range(numCourses): 
+            adj[i] = [] 
+        for src, dst in prerequisites: 
+            adj[src].append(dst) 
+        
+        visit = set() 
+        path = set() # list of nodes in the path 
+        topSort = [] # Add a topological sort container
+        for i in range(numCourses): 
+            if not self.dfs(i, adj, visit, path, topSort): 
+                return []
+        return topSort
+
+    def dfs(self, src, adj, visit, path, topSort): 
+        # if we find the src node again in the node we return 
+        if src in path: 
+            return False
+        if src in visit: 
+            return True 
+
+        visit.add(src) # Add to the visit set
+        path.add(src) # Add it to the path
+
+        all_paths = True 
+        for neighbor in adj[src]: 
+            all_paths = all_paths & self.dfs(neighbor, adj, visit, path, topSort)
+            if not all_paths: 
+                return False
+        path.remove(src) # Remove the src node from the path (post-order)
+        topSort.append(src) # Add to the topological sort (post-order)
+        return all_paths 
+        
