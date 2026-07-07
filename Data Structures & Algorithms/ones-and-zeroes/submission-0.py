a2013386c95270c7df3a4954e41f9918ef35680b
Daniel Sin <67078201+dsin85691@users.noreply.github.com>
2026-07-07 17:08:47 -0400

    Add: ones-and-zeroes - submission-0

---
+++ Data Structures & Algorithms/ones-and-zeroes/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for s in strs:
+            zeros, ones = s.count("0"), s.count("1")
+            for j in range(m, zeros - 1, -1):
+                for k in range(n, ones - 1, -1):
+                    dp[j][k] = max(dp[j][k], 1 + dp[j - zeros][k - ones])
+        return dp[m][n]
