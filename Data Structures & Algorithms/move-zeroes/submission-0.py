49fac78ff7d4df7e56cd42413cd12d1b262d6a14
Daniel Sin <67078201+dsin85691@users.noreply.github.com>
2026-08-18 16:34:32 -0400

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l, r = 0, 1
+        while r < len(nums): 
+            if nums[l] == 0:
+                if abs(nums[l]) < abs(nums[r]): 
+                    tmp = nums[r] 
+                    nums[r] = nums[l]
+                    nums[l] = tmp 
+                    print(l,r)
+                    # Swap 
+                    l += 1 # Move l pointer
+            else: 
+                l += 1 # Move 1 pointer (skip over it)
+            r += 1 # Move upwards to maintain order
